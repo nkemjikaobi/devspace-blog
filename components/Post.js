09be95c448adb7d581b94b4f1dfe672dc01f3168
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-12 14:14:35 +0100

    Category Label Component

--- components/Post.js
+++ components/Post.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import CategoryLabel from 'components/CategoryLabel';
 
 const Post = ({ post }) => {
 	return (
@@ -17,7 +18,7 @@ const Post = ({ post }) => {
 				<span className='font-light text-gray-600'>
 					{post.frontmatter.date}
 				</span>
-				<div>{post.frontmatter.category}</div>
+				<CategoryLabel>{post.frontmatter.category}</CategoryLabel>
 			</div>
 			<div className='mt-2'>
 				<Link href={`/blog/${post.slug}`}>
