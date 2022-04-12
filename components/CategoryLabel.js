ec03e97d67326c299680705408bb49c387fb8cc0
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-12 16:31:24 +0100

    Categories and Pagination

--- components/CategoryLabel.js
+++ components/CategoryLabel.js
@@ -9,10 +9,6 @@ export default function CategoryLabel({ children }) {
 		Ruby: 'red',
     };
 
-    console.log({children})
-    
-    console.log(colorKey[children])
-
 	return (
 		<div
 			className={`px-2 py-1 bg-${colorKey[children]}-600 text-gray-100 font-bold rounded`}
